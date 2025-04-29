6e68ddd960ec84b9bac414c0c4152f2d59dc01f3
Ayush-Ach <home48591@gmail.com>
2025-04-29 14:21:07 -0400

    Update final.py

--- final.py
+++ final.py
@@ -4,15 +4,22 @@ import turtle
 from turtle import RawTurtle, ScrolledCanvas, TurtleScreen
 from math import floor
 
-#############################################################
-#Displays the instructions on the screen for the players to see. 
-#(Kapishh Rajan)
-print("Welcome to Ultimate tic-tac-toe, this is a 2 player game where your goal is to place your markers (X and 0) into your desired positions to try and get 3 in a row.")
-print("You can either try 3 in a row horizontally, 3 in a row vertically, or 3 in a row diagonaly")
-print("But this is ULTIMATE tic-tac-toe, its not just one grid, now you got 6 grids that you have to fill out.")
-print("Your goal is not only to get 3 in a row and win the grid, but also win 3 grids in a row in the same fashion as you would win the grids")
-print("The move you pick in the grid will correspond to the grid your opponant will start in.")
-print("May be best player win!!!!")
+root = tk.Tk()
+
+root.title("Ultimate Tic Tac Toe")
+root.configure(background="white")
+root.minsize(1000, 1000)
+root.maxsize(1200, 1200)
+root.geometry("1200x1200+1200+100")
+
+canvas = tk.Canvas(master = root, width = 1200, height = 1200)
+canvas.grid(padx=2, pady=2, row=0, column=0, rowspan=10, columnspan=10)
+
+
+screen = turtle.TurtleScreen(canvas)
+c = turtle.RawTurtle(screen)
+
+screen.tracer(0)
 
 ##################################################################################################################################################################################################################################################################################################
 #CODE FOR THE BOARD ~AYUSH
@@ -70,24 +77,27 @@ def display():
 
 
 
-
+anygrid = True
 ####### Move function creates an X or O in an empty space #########
 def move(list_grid, box):
     global grid
+    global anygrid
     global player
 
-    if list_grid != grid and grid != 9:
+    if list_grid != grid and not anygrid:
         print("Invalid input")
 
     elif board[list_grid][box] == 0:
         board[list_grid][box] = player
         check(list_grid)
         grid = box
+        anygrid = False
         display()
         if meta[box] != 0:
-            list_grid = int(input('Choose any grid'))
+            anygrid = True
         player %= 2
         player += 1
+        print('P'+str(player)+': ')
     else:
         display()
         print('Square Taken')
@@ -108,51 +118,14 @@ def check(list_grid):
 
 
     for i in [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]]:
-        if meta[i[0]] == board[i[1]] == board[i[2]] != 0:
+        if meta[i[0]] == meta[i[1]] == meta[i[2]] != 0:
             print('P' + str(player) + 'Wins')
+            quit
 
 
 
 
 display()
-
-
-
-#####################################################################
-# this is the old conditional statment
-# while True:
-#     try:
-#         player %= 2
-#         player += 1
-
-#         print('PLAYER ' + str(player))
-#         player_move= move(int(input('Pick a Number 1-9 : '))-1)
-#     except:
-#         display()
-#         print('Invalid Input')
-
-
-######################################
-#Re worked conditionals (Kapishh Rajan)
-
-'''while True:
-    try:
-        # Alternate between players 1 and 2
-        player %= 2
-        player += 1
-
-        print('PLAYER ' + str(player))
-
-        # Validate input and ensure it's within the range 1-9
-        player_input = int(input('Pick a Number 1-9: '))
-        while not (1<= int(player_input) <= 9):
-            player_input = int(input('Input must be between 1 and 9. Pick a Number 1-9: '))
-
-        player_move = move(int(input('grid: ')), int(player_input) - 1)
-
-### checks if there are any errors in the input
-    except:
-        print("Invalid Input")'''
 
 
 ##################################################################################################################################################################################################################################################################################################
@@ -185,18 +158,6 @@ def draw_circle(x, y):
     c.penup()
 
 
-root = tk.Tk()
-
-root.title("Ultimate Tic Tac Toe")
-root.configure(background="white")
-root.minsize(1000, 1000)
-root.maxsize(1200, 1200)
-root.geometry("1200x1200+1200+100")
-canvas = tk.Canvas(master = root, width = 1200, height = 1200)
-canvas.grid(padx=2, pady=2, row=0, column=0, rowspan=10, columnspan=10)
-
-c = turtle.RawTurtle(canvas)
-c.speed(0)
 c.penup()
 c.pensize(15)
 
@@ -214,32 +175,31 @@ placeholder = tk.PhotoImage()
 # Ayush
 def jp_move(row, col):
     global player
-    player %= 2
-    player += 1
     list_grid, box = (floor(col/3)+3*floor(row/3), col%3 + row%3*3)
     player_move = move(list_grid, box)
+    updategui()
+    screen.update()
 
 def updategui():
-    global row
-    global col
-    
-    replace(row, col)
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 1:
+                replace(3*(i%3) + j%3, 3*floor(i/3) + floor(j/3), 1)
+            elif board[i][j] == 2:
+                replace(3*(i%3) + j%3, 3*floor(i/3) + floor(j/3), 2)
 
 # Hamza
-def replace(row, col):
-    global player
+def replace(col, row, p):
     i=row * 9 + col 
     button = buttons[i]
 # destroys the button and places an X or an O in the empty space
-    if player == 1:
+    if p == 1:
         button.destroy()
         draw_x((col-4) * 135,(row-4) * -135)     
     else:
         button.destroy()
         draw_circle((col-4) * 135,(row-4) * -135)
 # changes the active player
-    player+=1
-    player%=2
 
 # Hamza - CITE SOURCES
 for row in range(9):
