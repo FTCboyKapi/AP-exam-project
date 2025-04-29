0a72b9cac08a34925254b7783f8bfb7a843bf537
Ayush-Ach <home48591@gmail.com>
2025-04-29 14:13:19 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
-# -*- coding: utf-8 -*-
-# Ultimate Tic Tac Toe
+# Hamza/Yousuf
+import tkinter as tk
+import turtle
+from turtle import RawTurtle, ScrolledCanvas, TurtleScreen
+from math import floor
 
+root = tk.Tk()
+
+root.title("Ultimate Tic Tac Toe")
+root.configure(background="white")
+root.minsize(1000, 1000)
+root.maxsize(1200, 1200)
+root.geometry("1200x1200+1200+100")
+
+canvas = tk.Canvas(master = root, width = 1200, height = 1200)
+canvas.grid(padx=2, pady=2, row=0, column=0, rowspan=10, columnspan=10)
+
+
+screen = turtle.TurtleScreen(canvas)
+c = turtle.RawTurtle(screen)
+
+screen.tracer(0)
+
+##################################################################################################################################################################################################################################################################################################
 #CODE FOR THE BOARD ~AYUSH
-################################################################################
+
 board = [[0,0,0, 0,0,0, 0,0,0],  [0,0,0, 0,0,0, 0,0,0],  [0,0,0, 0,0,0, 0,0,0],
          [0,0,0, 0,0,0, 0,0,0],  [0,0,0, 0,0,0, 0,0,0],  [0,0,0, 0,0,0, 0,0,0],
          [0,0,0, 0,0,0, 0,0,0],  [0,0,0, 0,0,0, 0,0,0],  [0,0,0, 0,0,0, 0,0,0]]
@@ -32,55 +53,53 @@ grid=4
 
 # Display will become irrelevant when a GUI is made
 # List comprehension: https://docs.python.org/3/glossary.html#term-list-comprehension
-
-############################################################# 
-###Terminal display### 
-
-# def display():
-#     global board
-#     global grid
-
-
-
-#     dp = '-----------------------------\n'
-#     board[grid] = ['⁕' if i == 0 else i for i in board[grid]]
-   
-#     for i in range(3):
-#         for j in range(3):
-#             for k in range(3):
-#                 dp += '| '
-#                 for l in range(3):
-#                     dp += str(board[3*i+k][3*j+l]) + ' '
-#                 dp += '| '
-#             dp += '\n'
-#         dp += '-----------------------------\n'
-   
-#     print(dp.replace('0', '•').replace('1', 'X').replace('2', 'O'))
-#     board[grid] = [0 if i == '⁕' else i for i in board[grid]]
-
-
-
-
-####### Move function creates an X or O in an empty space #########
-def move(box):
+def display():
+    global board
     global grid
+
+
+
+    dp = '-----------------------------\n'
+    if grid!=9: board[grid] = ['⁕' if i == 0 else i for i in board[grid]]
+   
+    for i in range(3):
+        for j in range(3):
+            for k in range(3):
+                dp += '| '
+                for l in range(3):
+                    dp += str(board[3*i+k][3*j+l]) + ' '
+                dp += '| '
+            dp += '\n'
+        dp += '-----------------------------\n'
+   
+    print(dp.replace('0', '•').replace('1', 'X').replace('2', 'O'))
+    board[grid] = [0 if i == '⁕' else i for i in board[grid]]
+
+
+
+anygrid = True
+####### Move function creates an X or O in an empty space #########
+def move(list_grid, box):
+    global grid
+    global anygrid
     global player
 
+    if list_grid != grid and not anygrid:
+        print("Invalid input")
 
-
-
-    if board[grid][box] == 0:
-        board[grid][box] = player
-        check(grid)
+    elif board[list_grid][box] == 0:
+        board[list_grid][box] = player
+        check(list_grid)
         grid = box
-        player += 1
-        player %= 2
-
-        # display()
+        anygrid = False
+        display()
         if meta[box] != 0:
-            grid = int(input('Choose any grid'))
+            anygrid = True
+        player %= 2
+        player += 1
+        print('P'+str(player)+': ')
     else:
-        # display()
+        display()
         print('Square Taken')
 
 
@@ -105,60 +124,96 @@ def check(list_grid):
 
 
 
-# display()
+display()
 
 
+##################################################################################################################################################################################################################################################################################################
 
-#####################################################################
-# this is the old conditional statment (Kapishh Rajan)
-# while True:
-#     try:
-#         player %= 2
-#         player += 1
+def draw_x(x, y):
+    c.penup()
+    c.goto(x,y)
+    c.color('red')
 
-#         print('PLAYER ' + str(player))
-#         player_move= move(int(input('Pick a Number 1-9 : '))-1)
-#     except:
-#         display()
-#         print('Invalid Input')
-
-
-######################################
-#Re worked conditionals (Kapishh Rajan)
-
-while True:
-    try:
-        # Alternate between players 1 and 2
-        player %= 2
-        player += 1
-
-        print('PLAYER ' + str(player))
+    c.pendown()
+    c.setheading(45)
+    c.forward(70)
+    c.back(140)
+    c.forward(70)
     
-        # Validate input and ensure it's within the range 1-9
-        player_input = int(input('Pick a Number 1-9: '))
-        if not (1<= int(player_input) <= 9):
-            print("input must be between 1 and 9")
-        
-        player_move = move(int(player_input) - 1)
+    c.right(90)
+    c.forward(70)
+    c.back(140)
+    c.penup()
 
-### checks if there are any errors in the input
-
-    except ValueError as value_error:
-        print(f"Invalid Input: {value_error} - Please enter a number between 1 and 9.")
-        
-    except IndexError as index_error:
-        print(f"Invalid Input: {index_error} - Please enter a number between 1 and 9.")
-
-    except Exception as exception:
-        # display()
-        print(f"An error occurred: {exception} - Please try again.")
-
-#Keeps the player number if there is any error present 
-    if ValueError or IndexError or Exception == True:
-        player %= 2
-        player += 1
-
-
-
+def draw_circle(x, y):
     
+    c.penup()
+    c.setheading(0)
+    c.goto(x, y - 50) 
+    c.color('blue')
 
+    c.pendown()
+    c.circle(50)  
+    c.penup()
+
+
+c.penup()
+c.pensize(15)
+
+# Ayush- drawing thick border lines
+for i in range(4):
+    c.setheading([0, 0, 90, 90][i])
+    c.goto([-600, -600, 135*1.5, -135*1.5][i], [135*1.5, -135*1.5, -600, -600][i])
+    c.pendown()
+    c.forward(1200)
+    c.penup()
+
+buttons = []
+placeholder = tk.PhotoImage()
+
+# Ayush
+def jp_move(row, col):
+    global player
+    list_grid, box = (floor(col/3)+3*floor(row/3), col%3 + row%3*3)
+    player_move = move(list_grid, box)
+    updategui()
+    screen.update()
+
+def updategui():
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] == 1:
+                replace(3*(i%3) + j%3, 3*floor(i/3) + floor(j/3), 1)
+            elif board[i][j] == 2:
+                replace(3*(i%3) + j%3, 3*floor(i/3) + floor(j/3), 2)
+
+# Hamza
+def replace(col, row, p):
+    i=row * 9 + col 
+    button = buttons[i]
+# destroys the button and places an X or an O in the empty space
+    if p == 1:
+        button.destroy()
+        draw_x((col-4) * 135,(row-4) * -135)     
+    else:
+        button.destroy()
+        draw_circle((col-4) * 135,(row-4) * -135)
+# changes the active player
+
+# Hamza - CITE SOURCES
+for row in range(9):
+    root.rowconfigure(row,minsize=100, weight=1)
+    
+    for column in range(9):
+        
+        root.columnconfigure(column,minsize=100, weight=1)
+        button = tk.Button(root,image=placeholder, width= 100, height = 100,command= lambda row =  row, col = column : jp_move(row, col))
+        button.grid(column=column, row=row)
+        buttons.append(button)
+##################################################################################################################################################################################################################################################################################################
+
+# -*- coding: utf-8 -*-
+# Ultimate Tic Tac Toe
+
+
+root.mainloop()
